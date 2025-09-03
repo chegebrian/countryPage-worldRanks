0470021d3b9ca8bd8a442e5cc37207ff39f26903
brianwaithaka <waithakabrian8@gmail.com>
2025-09-03 12:07:59 +0300

    make the borders navigatable

--- src/components/NeighbourCountries.jsx
+++ src/components/NeighbourCountries.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useWorldRanksApi } from '../contexts/WorldRanksApi'
+import { NavLink } from 'react-router';
 
 function NeighbourCountries({ border }) {
   const { countries } = useWorldRanksApi()
 
   const findNeighbour = countries?.find((country) => country.cca3 === border)
   console.log(findNeighbour);
-  
+
   return (
-    <div className='flex flex-col gap-2'>
-      <img src={findNeighbour.flags.svg} alt={findNeighbour.flags.alt} className='w-24 rounded-lg'/>
+    <NavLink to={`/worldRanks/${findNeighbour.name.common}`} className='flex flex-col gap-2'>
+      <img src={findNeighbour.flags.svg} alt={findNeighbour.flags.alt} className='w-24 rounded-lg' />
       <span>{findNeighbour.name.common}</span>
-    </div>
+    </NavLink>
   )
 }
 
